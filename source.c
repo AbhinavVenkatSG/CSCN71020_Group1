869fb540a769b89c1d9c22e44d91629e4211f0de
Rami Musleh <Rmusleh8054@conestogac.on.ca>
2025-03-11 17:34:37 -0400

    Triangle function 1.1 (Basic function)

--- source.c
+++ source.c
@@ -3,38 +3,57 @@
 #include <math.h>
 
 void get_triangle_input() {
-
+    printf("Triangle\n\n");
+  
     // Function 1:
-    // Separate function to get side length input from user as floats
-    // Separate function to validate, no characters, no special characters, no neg numbers
+  // Separate function to get side length input from user as floats
+  // Separate function to validate, no characters, no special characters, no neg numbers
 
-    // initialize variables as floats
-    float sideA = 0.0;
-    float sideB = 0.0;
-    float sideC = 0.0;
-
-
-
-    // Validate Input
-
-    
-    
+  // initialize variables as floats
+    float sideA = 0;
+    float sideB = 0;
+    float sideC = 0;
 
 
-	// Function 2:
+// Side A
+
+   // Side A Input
+    printf("Enter the first side: ");
+    int sideASymbols = scanf("%f", &sideA);
+   
+   // Side B Input
+    printf("Enter the second side: ");
+    int sideBSymbols = scanf("%f", &sideB);
+
+   // Side C Input
+    printf("Enter the third side: ");
+    int sideCSymbols = scanf("%f", &sideC);
+
+    // Validate
+    if (sideASymbols != 1 || sideA < 0 || sideBSymbols != 1 || sideB < 0 || sideCSymbols != 1 || sideC < 0) {
+        printf("Invalid Input.\n");
+    }
+
+    else {
+        printf("Jasons function should be here");
+        // Call Jasons Function
+    }
+
+    // Function 2:
     // Final function to calculate the angle
 
 }
 
+
 void get_rectangle_input() {
-    int test;
+    printf("Rectangle\n");
 
 
 }
 
 int main() {
     int choice;
-    while (1) {
+    //while (1) {
         printf("\nMenu:\n1. Triangle Feature\n2. Rectangle Feature\n3. Exit\n");
         printf("Enter your choice: ");
         scanf_s("%d", &choice);
@@ -45,11 +64,11 @@ int main() {
             get_rectangle_input();
         }
         else if (choice == 3) {
-            break;
+           // break;
         }
         else {
             printf("Invalid choice. Try again.\n");
         }
-    }
+   // }
     return 0;
 }
