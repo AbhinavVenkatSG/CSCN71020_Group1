b5bc70a4293d4ff955c49876fd20fc132a844b37
Jason Little <Jlittle6810@conestogac.on.ca>
2025-03-13 13:22:10 -0400

    updated the validatetriangle.c to return true

--- validatetriangle.c
+++ validatetriangle.c
@@ -4,6 +4,15 @@
 
 bool validateOfTriangle(float sideA, float sideB, float sideC) {
 
-   
+    //Validate
+    if (sideA + sideB > sideC && sideB + sideC > sideA && sideA + sideC > sideB) {
+
+        return true;
+
+    else {
+
+        printf("Not a Triangle");
+
+    }
 
 }
