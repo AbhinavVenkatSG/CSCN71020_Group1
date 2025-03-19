f24ef99ea0bd234439c674fa9aa506340ef5f1c2
Abhinav Venkat Sivakumar Gayathiri <Asivakumargayat1253@conestogac.on.ca>
2025-03-19 11:42:40 -0400

    .

--- source.c
+++ source.c
@@ -5,7 +5,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include "validatetriangle.h"
+//#include "validatetriangle.h"
 
 #define NUMLIMIT 100
 char sideA[NUMLIMIT];
@@ -13,8 +13,44 @@ char sideB[NUMLIMIT];
 char sideC[NUMLIMIT];
 
 // OK, so I made sure the code is structured properly, but I'm still getting multiple errors in linking (Just KILL me atp).
-// One of the errors was a typo in the include this  took me 2 hours to figure that one out.
+// One of the errors was a typo in the include took me 2 hours to figure that one out.
 // The code runs in a separate project, but for some stupid reason, it decided not to work here.
+//For my own insanity i am going have the function in this main.c file apprantly the program is having problem in link our .h file.
+//So if the function is in main it work perfectly fine code safe against most of the test cases we discussed include the double decimal 
+
+bool get_triangle_input(char* side) {
+    int dot_counter = 0;
+    int length = strlen(side);
+    if (length == 0) {
+        printf("Invalid Input: Empty input\n");
+        return false;
+    }
+    for (int i = 0; i < length; i++) {
+        if (isdigit(side[i])) {
+            continue;
+        }
+        else {
+            printf("Please Enter a Number as Input ");
+                return false;
+        }
+        if (side[i] == '.') {
+            dot_counter++;
+
+            if (dot_counter > 1) {
+                printf("Invalid Input: More than one decimal point\n");
+                return false;
+            }
+            continue;
+        }
+
+
+
+        printf("Invalid Input: Non-numeric character\n");
+        return false;
+    }
+    return true;
+}
+
  int main() {
         int choice;
 
@@ -53,9 +89,6 @@ char sideC[NUMLIMIT];
                     return false;
 
                 }
-
-
-
             }
             else if (choice == 2) {
                 // Rectangle functionality (not implemented)
